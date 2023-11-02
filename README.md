54d8243cfd6fd6228d191469e6a5cd281f149d64
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-02 11:37:00 -0700

    add columns and css code

--- README.md
+++ README.md
@@ -1,28 +1,54 @@
+# Pig Dice Game project.
+#### By: **Zuri Gallegos**
+_*Week 4 Epicodus*_
+
+## Project Description: 
+This web program is a game where two users can play Pig dice against each other.
+
 ## TDD
 ### Describe: Program where two users can play Pig dice against each other.
 
-Test: “It should create a function for Pig Dice”
-Code: function PigDiceGame() 
+Test: “It should create a function for Pig Dice”\
+Code: function PigDiceGame()\
 Output: 
 
-Test: “It should generate a random number 1-6”
-Code: Math.floor(Math.random() * 6) + 1
+Test: “It should generate a random number 1-6”\
+Code: Math.floor(Math.random() * 6) + 1\
 Output: 1-6
 
-Test: “it should reset score to zero if 1 comes up and switch to the new player”
-Code: activePlayer() switchPlayer()
+Test: “it should reset score to zero if 1 comes up and switch to the new player”\
+Code: activePlayer() switchPlayer()\
 Output:
 
-Test: “It should determine the "winner" if active player scores 100 or more”
-Code: totalScore >= 100
+Test: “It should determine the "winner" if active player scores 100 or more”\
+Code: totalScore >= 100\
 Output: "Player 1 wins"
 
-Test: “It should store score for players after each dice roll”
-Code: rollDice() currentScore()
+Test: “It should store score for players after each dice roll”\
+Code: rollDice() currentScore()\
 Output:
 
-Test: “It should use hold button to switch to new player”
-Code: switchPlayer()
+Test: “It should use hold button to switch to new player”\
+Code: switchPlayer()\
 Output:
 
+## Setup/Installation:
+* _You can clone this repository, you can do this by going to this link https://github.com/ZuriGa/Pig-Dice-Game.git_
+* _follow the link above and click on the <> Code button then click on the copy button. Wherever you are storing your projects on your computer, use this git command: git clone url-of-remote-repo. A new folder should be created on your computer and you can navigate it now._
+* _You can also fork this repository by clicking the "Fork" button in the remote repository._ 
+![My_Image](img/fork.jpng.png)\
+Click "create new fork" and then select your account as the "Owner", after this you have your own copy of this repository to your GitHub Account.
 
+## Technology used:
+* _JavaScript_
+* _HTML_
+* _CSS_
+
+## Known Bugs:
+* _No known Bugs_
+
+## License 
+
+MIT License
+
+Copyright (c) 2023 Zuri Gallegos
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+// Business Logic
 function PigDiceGame() {
     this.players = [
-    { name: "Player 1", totalScore: 0, currentScore: 0},
-    { name: "Player 2", totalScore: 0, currentScore: 0}
+        { name: "Player 1", totalScore: 0, currentScore: 0 },
+        { name: "Player 2", totalScore: 0, currentScore: 0 }
     ];
-    this.activePlayer = 0; 
+    this.activePlayer = 0;
     this.dice = 0;
 }
 
@@ -22,34 +23,91 @@ PigDiceGame.prototype.rollDice = function() {
 PigDiceGame.prototype.hold = function() {
     this.players[this.activePlayer].totalScore += this.players[this.activePlayer].currentScore;
 
-    if (this.players[this.activePlayer].totalScore >= 100){
-        return "winner";
+    if (this.players[this.activePlayer].totalScore >= 100) {
+        return this.players[this.activePlayer].name;
     }
 
     this.players[this.activePlayer].currentScore = 0;
     this.switchPlayer();
-    return this.activePlayer;
-
+    return null;
 };
 
 PigDiceGame.prototype.switchPlayer = function () {
-
     this.activePlayer = 1 - this.activePlayer;
 };
 
 PigDiceGame.prototype.getCurrentPlayer = function () {
-    return this.activePlayer;
-}
+    return this.players[this.activePlayer];
+};
 
 PigDiceGame.prototype.getCurrentDice = function () {
     return this.dice;
 };
 
 PigDiceGame.prototype.reset = function () {
-    this.activePlayer = [
-        {name: "Player 1", totalScore: 0, currentScore: 0},
-        {name: "Player 2", totalScore: 0, currentScore: 0}
+    this.players = [
+        { name: "Player 1", totalScore: 0, currentScore: 0 },
+        { name: "Player 2", totalScore: 0, currentScore: 0 }
     ];
     this.activePlayer = 0;
     this.dice = 0;
-}
+};
+
+// UI Logic
+window.addEventListener("load", function() {
+    const game = new PigDiceGame();
+    const rollButton = document.getElementById("buttonRoll");
+    const holdButton = document.getElementById("buttonHold");
+    const newGameButton = document.getElementById("buttonNew");
+    const playerNameElement = document.getElementById("player1");
+    const totalScoreElement1 = document.getElementById("total-score-1");
+    const totalScoreElement2 = document.getElementById("total-score-2");
+    const currentScoreElement1 = document.getElementById("current-score-1");
+    const currentScoreElement2 = document.getElementById("current-score-2");
+    const messageElement = document.getElementById("message");
+
+    function updateUI() {
+        const currentPlayer = game.getCurrentPlayer();
+        playerNameElement.textContent = currentPlayer.name + "'s Turn";
+        if (currentPlayer.name === "Player 1") {
+            document.getElementById("player1").textContent = "Players 1's Turn";
+            document.getElementById("player2").textContent = "Player 2";
+        } else {
+            document.getElementById("player1").textContent = "Player 1";
+            document.getElementById("player2").textContent = "Players 2's Turn";
+        }
+        totalScoreElement1.textContent = game.players[0].totalScore;
+        totalScoreElement2.textContent = game.players[1].totalScore;
+        currentScoreElement1.textContent = game.players[0].currentScore;
+        currentScoreElement2.textContent = game.players[1].currentScore;
+        messageElement.textContent = "";
+    }
+
+    rollButton.addEventListener("click", function () {
+        const diceValue = game.rollDice();
+        updateUI();
+
+        if (diceValue === 1) {
+            messageElement.textContent = "Oops! You rolled 1";
+        }
+
+    });
+
+    holdButton.addEventListener("click", function () {
+        const winner = game.hold();
+        updateUI();
+
+        if (winner) {
+            messageElement.textContent = winner + " wins!";
+        }
+
+    });
+
+    newGameButton.addEventListener("click", function () {
+        game.reset();
+        updateUI();
+    });
+
+    updateUI();
+
+});
